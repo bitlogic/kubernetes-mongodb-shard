3e165100248f2ab19ec71c063e90710ac34fc754
Gabriel Miretti <gabriel.miretti@gmail.com>
2018-09-24 12:30:53 -0300

    Making configurable mongo version and data directory

--- src/configure.sh
+++ src/configure.sh
@@ -2,8 +2,10 @@
 
 set -eo pipefail
 
+LC_ALL="C"
 NODES=$1 && [ -z "${1}" ] && NODES="3" || true
-VERSION="3.2"
+MONGO_VER="3.6.7"
+DATADIR="/usr/local/mongo/data"
 
 echo
 echo "Please supply some important configuration parameters below:"
@@ -12,10 +14,10 @@ read -p "SSH user for cluster access [root]: " SSHUSER
 [ -z "$SSHUSER" ] && SSHUSER="root" || true
 read -p "SSH port for cluster access [22]: " SSHPORT
 [ -z "$SSHPORT" ] && SSHPORT="22" || true
-read -p "Root directory for your mongodb data [/data]: " BASEDIR
-[ -z "$BASEDIR" ] && BASEDIR="/data" || true
+read -p "Root directory for your mongodb data [${DATADIR}]: " BASEDIR
+[ -z "$BASEDIR" ] && BASEDIR=$DATADIR || true
 read -p "Number of nodes for your shard [${NODES}]: " CFGNODES
 [ -z "$CFGNODES" ] && CFGNODES=$NODES || true
-read -p "MongoDB version [${VERSION}]: " VERSION
-[ -z "$VERSION" ] && VERSION="3.2" || true
+read -p "MongoDB version [${MONGO_VER}]: " VERSION
+[ -z "$VERSION" ] && VERSION=$MONGO_VER || true
 echo
